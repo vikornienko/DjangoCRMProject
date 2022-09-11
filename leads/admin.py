69e4df8130d83d314135edd98ff8c491caf2b661
vikornienko <vikornienko76@gmail.com>
2022-09-11 17:39:21 +0500

    Twelfth commit.
    Model display. Registration of models.

--- leads/admin.py
+++ leads/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import User, Lead, Agent
+
+
+admin.site.register(User)
+admin.site.register(Lead)
+admin.site.register(Agent)
